f08999856e9e6a3b5bac9e1296a60bbf9a34d18f
capitainecl <guillaume.poirier@clinia.ca>
2020-03-23 14:24:45 -0400

    feat: Added example query

---
+++ ntp-playground/NTPPlayground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using GuerrillaNtp;
+
+namespace NTPPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // query the SNTP server
+            NtpPacket response;
+            using (var ntp = new NtpClient(Dns.GetHostAddresses("pool.ntp.org")[0]))
+                response = ntp.Query();
+            // offset = ntp.GetCorrectionOffset();
+            
+            Console.WriteLine(response.CorrectionOffset);
+        }
+    }
+}
